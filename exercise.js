b760724968f474b4240bef99572ea995ef5674c5
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-11-27 13:26:36 -0600

    create a function reverse the Number

--- exercise.js
+++ exercise.js
@@ -6,3 +6,33 @@ const printString = function (name) {
   }
 };
 console.log(printString("Zaka"));
+
+//Create a Function and console the string in reverse order
+
+const reverseString = function (name) {
+  for (i = name.length - 1; i >= 0; i--) {
+    console.log(name[i]);
+  }
+};
+console.log(reverseString("Khan"));
+
+// create a function which print the numbers
+// let p = 23423423;
+// console.log(p);
+
+// let number = 123456789;
+// let text = number.toString().split(",").join("");
+// console.log(typeof text);
+
+// for (i = text.length - 1; i >= 0; i--) {
+//   console.log(text[i]);
+// }
+
+// let number = 123456789;
+const reverseNumber = function (number) {
+  let text = number.toString().split(",").join("");
+  for (i = text.length - 1; i >= 0; i--) {
+    console.log(text[i]);
+  }
+};
+console.log(reverseNumber(12345678999));
