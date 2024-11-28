bd78f5b81151c68ffd0d7bc6add208ed7800fec2
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-11-28 17:54:40 -0600

    Array using maping function

--- array1.js
+++ array1.js
@@ -1,1 +1,7 @@
 const friends = ["Zaka", "Tayyab,", "Ahmad", "Shan Zeb"];
+
+console.log(friends);
+
+const ot = friends.map((currentElement, index) => currentElement + " " + index);
+
+console.log(ot);
