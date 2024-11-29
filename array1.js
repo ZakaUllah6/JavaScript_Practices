2d9d4647322f36811fa223cdd4f407d0cd8a15b7
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-11-28 18:03:03 -0600

    Array using maping function

--- array1.js
+++ array1.js
@@ -5,3 +5,11 @@ console.log(friends);
 const ot = friends.map((currentElement, index) => currentElement + " " + index);
 
 console.log(ot);
+
+const age = function (years) {
+  return 2037 - years;
+};
+
+const years = [1999, 1988, 1976, 1966];
+
+console.log(age(years[0]));
