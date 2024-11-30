6be2151c4284bd67aeee2674548f2e691ee7a4c2
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-11-29 23:13:09 -0600

    How to add objects in object through help with dot notation and bracket notation

--- object.js
+++ object.js
@@ -5,13 +5,24 @@ const zaka = {
   friends: ["Ahmad", "Sheraz", "Tayyab"],
 };
 console.log(zaka);
-// Accessing object through dot notation we cant accessing compute variable for compute we should use object expression
+// Accessing object through dot notation we cant accessing compute variable for compute we should use object bracket notation
 console.log(zaka.firstName);
 console.log(zaka.friends);
 
-// trying to accessing the object through object Expression method
+// trying to accessing the object through Bracket notation
 console.log(zaka["lastName"]);
 const nameKey = "Name";
 
 console.log(zaka["first" + nameKey]);
 console.log(zaka["last" + nameKey]);
+
+// How to add new objects in object through dot notation
+
+zaka.location = "USA";
+console.log(zaka);
+
+//How to add new objects in object with the of bracket notation
+
+zaka["job"] = "Software Engineering";
+
+console.log(zaka);
