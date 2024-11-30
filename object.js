92f11f9af2e1155f890864e3358f340a22d03139
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-11-29 22:52:57 -0600

    Accessing the object through Object Expression

--- object.js
+++ object.js
@@ -8,3 +8,10 @@ console.log(zaka);
 // Accessing object through dot notation we cant accessing compute variable for compute we should use object expression
 console.log(zaka.firstName);
 console.log(zaka.friends);
+
+// trying to accessing the object through object Expression method
+console.log(zaka["lastName"]);
+const nameKey = "Name";
+
+console.log(zaka["first" + nameKey]);
+console.log(zaka["last" + nameKey]);
