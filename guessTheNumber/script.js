d5b37d46be56b2b605ffda58237d391b8230450a
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-04 16:58:15 -0600

    Final Version of the game guess the number

--- guessTheNumber/script.js
+++ guessTheNumber/script.js
@@ -40,6 +40,28 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   }
 });
+console.log("");
+//   // When guess is too high
+//   else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(".message").textContent = "📈 Too High";
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//     } else {
+//       document.querySelector(".message").textContent = "You Lost The Game! 💥";
+//     }
+//   }
+//   //when guess too low
+//   else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(".message").textContent = "📉Too low";
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//     } else {
+//       document.querySelector(".message").textContent = "You Lost The Game! 💥";
+//     }
+//   }
+// });
 
 // Event listener for the "Again!" button to reset the game
 document.querySelector(".again").addEventListener("click", function () {
