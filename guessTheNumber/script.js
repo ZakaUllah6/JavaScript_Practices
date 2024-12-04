2ab099a89b851fe7b8cc428ac0ca9a95fcf773fd
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-04 16:19:22 -0600

    Final Version of game

--- guessTheNumber/script.js
+++ guessTheNumber/script.js
@@ -6,13 +6,14 @@
 // document.querySelector(".guess").value = 25;
 // console.log(document.querySelector(".guess").value);
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 // document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(typeof guess);
+  console.log(guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = " ❌ No Number Guessed.";
@@ -24,6 +25,15 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+    // if (score > hightscore) {
+    //   hightscore = score;
+    //   document.querySelector(".highscore").textContent = highscore;
+    // }
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
   // When guess is too high
   else if (guess > secretNumber) {
@@ -48,10 +58,11 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 
 document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
   score = 20;
   document.querySelector(".message").textContent = "Start Guesing....";
   document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").value = " ";
+  document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
 });
