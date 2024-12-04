dbf8c3eaa7d2c3b00f9310c1f51099e3f2b019d9
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-04 15:17:46 -0600

    Guessuing the number its give output when its too high or too low

--- guessTheNumber/script.js
+++ guessTheNumber/script.js
@@ -6,11 +6,40 @@
 // document.querySelector(".guess").value = 25;
 // console.log(document.querySelector(".guess").value);
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = " ❌ No Number Guessed.";
+  }
+  // When guess is true
+  else if (guess === secretNumber) {
+    document.querySelector(".message").textContent =
+      "The Guessed is correct! 🎉";
+  }
+  // When guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost The Game! 💥";
+    }
+  }
+  //when guess too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost The Game! 💥";
+    }
   }
 });
