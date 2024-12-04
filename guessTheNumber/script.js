b7601f9f90b1c1536ea2782215415dcb156ebae8
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-04 14:27:27 -0600

    guessed the Number

--- guessTheNumber/script.js
+++ guessTheNumber/script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Start Counting!";
+// document.querySelector(".number").textContent = 23;
+// document.querySelector(".guess").value = 25;
+// console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = " ❌ No Number Guessed.";
+  }
+});
