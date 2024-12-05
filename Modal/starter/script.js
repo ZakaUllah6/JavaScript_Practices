0583eb04356b96cff41b40c59dd3f693e7fc8d2a
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-04 22:25:49 -0600

    Now we close the modal with the help of keyboards key Escape

--- Modal/starter/script.js
+++ Modal/starter/script.js
@@ -4,6 +4,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal'); // Fixed class name
 const btnOpenModal = document.querySelectorAll('.show-modal');
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// }
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener('click', function () {
@@ -12,12 +21,13 @@ for (let i = 0; i < btnOpenModal.length; i++) {
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' || ('esc' && !modal.classList.contains('hidden'))) {
+    closeModal();
+  }
+  console.log(e);
 });
