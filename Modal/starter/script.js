e445182dc384f395f77a2f0eacad2f110e11df09
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-04 20:19:56 -0600

    Modal Using ClassList add and Remove

--- Modal/starter/script.js
+++ Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal'); // Fixed class name
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
