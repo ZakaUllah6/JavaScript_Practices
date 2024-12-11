25714bdc672ec4fddebc6297a3b0dca446723d8e
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-10 20:04:57 -0600

    Create a booking function and pass three parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPessenger = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPessenger,
+    price,
+  };
+  bookings.push(booking);
+  console.log(booking);
+};
+createBooking("Lh123");
