aeb68d09eb42c52cea062ec7a0b12180d0320a2e
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-11 11:11:33 -0600

    Set default values in parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,7 +2,11 @@
 
 const bookings = [];
 
-const createBooking = function (flightNum, numPessenger = 1, price = 199) {
+const createBooking = function (
+  flightNum,
+  numPessenger = 1,
+  price = 199 * numPessenger
+) {
   const booking = {
     flightNum,
     numPessenger,
@@ -11,6 +15,6 @@ const createBooking = function (flightNum, numPessenger = 1, price = 199) {
   bookings.push(booking);
   console.log(booking);
 };
-createBooking("Lh123");
-createBooking("Bus123");
-createBooking("Air123");
+createBooking("Lh1231", "1");
+createBooking("Bus1232", "2");
+createBooking("Air1235", "5");
