1504639981fdeebc17a1674e9c9ca4f1822c794f
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-19 21:06:49 -0600

    Doing the transfer and testing of transfering the test

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -239,5 +239,5 @@ const max = movements.reduce((acc, mov) => {
   if (acc > mov) return acc;
   else return mov;
 }, movements[0]);
-
+console.log(max);
 /////////////////////////////////////////////////
