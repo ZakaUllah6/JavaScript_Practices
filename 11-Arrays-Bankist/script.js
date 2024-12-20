1a905b1929c46f11a4d9e1a07141d42dc91f730d
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-19 20:25:02 -0600

    Transfer amount one person to another person

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -154,6 +154,17 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 //
+//Transfer Money one account to another account
+
+btnTransfer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  console.log(amount);
+  console.log(receiverAcc);
+});
 const euroToUSD = 1.1;
 
 const totalDepositUsd = account1.movements
