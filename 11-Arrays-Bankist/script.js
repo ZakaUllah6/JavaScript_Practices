71a646f7b3d02c670e5c51568decf5939cf946be
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-20 19:40:19 -0600

    Deleting an account functionality is complete

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -200,19 +200,43 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+// btnClose.addEventListener("click", function (e) {
+//   e.preventDefault();
+
+//   if (
+//     inputCloseUsername.value === currentAccount.username &&
+//     Number(inputClosePin.value) === currentAccount.pin
+//   ) {
+//     const index = accounts.findIndex(
+//       (acc) => acc.username === currentAccount.username
+//     );
+//     console.log(index);
+//   }
+// });
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin).value === currentAccount.pin
+    Number(inputClosePin.value) === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       (acc) => acc.username === currentAccount.username
     );
     console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
   }
+
+  inputCloseUsername.value = inputClosePin.value = "";
 });
+
 const euroToUSD = 1.1;
 
 const totalDepositUsd = account1.movements
