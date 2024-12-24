4155ada19077005fd2fb9440aa1ebbbbbe682ec5
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-24 11:13:39 -0600

    Selecting the elements

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -31,3 +31,17 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+//
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+document.querySelector(" header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+document.getElementById("section--1");
+allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+console.log(document.getElementsByCLassName("btn"));
