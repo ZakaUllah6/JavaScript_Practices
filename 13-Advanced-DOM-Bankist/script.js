ecc03f5a8550a65eef62efd1201b04304c8f65bf
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-25 16:34:48 -0600

    Alert is on mouseover

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -103,3 +103,11 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+//h1
+
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("mouseover", function (e) {
+  alert("Thats great your doing ");
+});
