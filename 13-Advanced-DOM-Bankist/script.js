0e63810e1bd73e15d7981fd127fa077b6c4faf94
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-25 22:10:48 -0600

    page navigation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -112,9 +112,27 @@ btnScrollTo.addEventListener("click", function (e) {
 //   alert("Thats great your doing ");
 // });
 
-querySelectorAll(".nav__links").addEventListener("click", function (e) {
+///////////////////////////////////////
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
-  if (e.target.classList.contains(".nav__link")) {
-    const id = e.target.getAttritube("href");
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
