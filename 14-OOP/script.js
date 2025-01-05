81c158e652e884eb6f43876e50e7a60abe70f699
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2025-01-05 14:44:38 -0600

    Practice OOP

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -8,3 +8,8 @@ const Person = function (firstName, birthYear) {
 
 const zaka = new Person("Zaka", 1997);
 console.log(zaka);
+
+const malidta = new Person("Malidta", 2999);
+const jonas = new Person("Jonas ", 1988);
+
+console.log(malidta, jonas);
