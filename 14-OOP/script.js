3f6f460df06c901bb236b7b606c2b0daa3a123db
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2025-01-05 14:56:52 -0600

    Dont use methods inside the constructor because its create deplicautes copies

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -5,17 +5,22 @@ const Person = function (firstName, birthYear) {
   //Instance Properties
   this.firstName = firstName;
   this.birthYear = birthYear;
+
+  //Never do this beacuse its create deplicautes copies
+  //   this.calcAge = function () {
+  //     console.log(2025 - birthYear);
+  //   };
 };
 
 const zaka = new Person("Zaka", 1997);
 console.log(zaka);
 
-// 1. New {}} is created
+// 1. New {} is created
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically return {}
 
-const malidta = new Person("Malidta", 2999);
+const malidta = new Person("Malidta", 1999);
 const jonas = new Person("Jonas ", 1988);
 
 console.log(malidta, jonas);
